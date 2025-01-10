3681fe833822d47bbe2bffa9bb907084fc79c9d5
b65t <iislamm6s7s5@tutamail.com>
2025-01-10 02:52:33 +0100

    Add files via upload

--- .config/scripts/swww-switcher.sh
+++ .config/scripts/swww-switcher.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-supported_formats=("jpeg" "png" "gif" "pnm" "tga" "tiff" "webp" "bmp" "farbfeld" "jpg")
 
 WALLPAPER_DIR=/home/islam/Background/
-CHOICE=$(ls "$WALLPAPER_DIR" -1 | grep \\.jpg | while read A ; do  echo -en "$A\x00icon\x1f~/Background/$A\n"; done | rofi -dmenu -p wall -theme ~/.config/rofi/swww/theme.rasi "Wallpaper")
+CHOICE=$(ls "$WALLPAPER_DIR" -1 | grep \\.jpg | while read A ; do  echo -en "$A\x00icon\x1f~/Background/$A\n"; done | rofi -dmenu -p " " -theme ~/.config/rofi/swww/theme.rasi "Wallpaper")
 WALLPAPER="$WALLPAPER_DIR/$CHOICE"
 
 if [[ -z $CHOICE ]]; then
