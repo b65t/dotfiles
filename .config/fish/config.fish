8ff2d48bd44c7b2486bdf3684fb2786d652b6544
b65t <iislamm6s7s5@tutamail.com>
2025-01-10 02:51:17 +0100

    Add files via upload

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,13 +1,12 @@
 if status is-interactive
 	set fish_greeting
-	pokemon-colorscripts -r
 end
 
 function fish_prompt
      set_color blue
         printf "-[%s]" (prompt_pwd)
         echo ' 
- '
+> '
 end
 
 alias ls="eza --icons"
@@ -15,3 +14,5 @@ alias tree="eza --icons --tree"
 alias cl="clear"
 alias nv="nvim"
 alias fm="yazi"
+
+starship init fish | source
