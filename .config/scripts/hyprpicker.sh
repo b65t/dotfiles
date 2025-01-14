9f94460f144a31cac3f0b3374c2fb1ce3320f760
b65t <iislamm6s7s5@tutamail.com>
2025-01-14 14:06:07 +0100

    Delete .config/scripts directory

--- .config/scripts/hyprpicker.sh
+++
@@ -1,8 +0,0 @@
-TMPPCK=$(hyprpicker -a -n)
-TMPPST=$(wl-paste)
-CMD=$(echo "$TMPPST")
-TMP=/tmp/xcolor_$CMD.png
-
-convert -size 120x120 xc:"$CMD" "$TMP"
-printf %s "$CMD" | wl-paste 
-notify-send -a "Color Picker" -i "$TMP" Color-picked! "$CMD"
--- .config/scripts/waypaper.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-wal -n -i "$1"
-$HOME/.cache/wal/colors.sh
-
-pkill swaync
-swaync
