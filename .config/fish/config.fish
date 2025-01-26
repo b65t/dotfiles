d4b45b33eed789dd274bd9c08b5eb8d163b3387e
b65t <iislamm6s7s5@tutamail.com>
2025-01-26 22:13:20 +0100

    Add files via upload

---
+++ .config/fish/config.fish
@@ -0,0 +1,18 @@
+if status is-interactive
+	set fish_greeting
+end
+
+function fish_prompt
+     set_color blue
+        printf "-[%s]" (prompt_pwd)
+        echo ' 
+> '
+end
+
+alias ls="eza --icons"
+alias tree="eza --icons --tree"
+alias c="clear"
+alias nv="nvim"
+alias fm="yazi"
+
+starship init fish | source
