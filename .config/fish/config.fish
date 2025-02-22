328be2bc65e08419b0534102e8886a83b6bf5c2f
b65t <iislamm6s7s5@tutamail.com>
2025-02-22 17:54:33 +0100

    Add files via upload

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,7 +3,11 @@ if status is-interactive
 end
 
 function fish_prompt
-printf '%s' (set_color normal) (set_color -b green) " " (set_color normal) (set_color -o black) (set_color -b green) (prompt_pwd) (set_color normal) (set_color normal) (set_color -o black) (set_color -b green) " ❯❯ " (set_color normal) " "
+printf  '%s' \
+   (set_color normal) (set_color -b green) " " \
+   (set_color normal) (set_color -o black) (set_color -b green) (prompt_pwd) \
+   (set_color normal) (set_color -o black) (set_color -b green) " ❯❯ " \
+   (set_color normal) " " 
 end
 
 alias ls="eza --icons"
@@ -11,7 +15,11 @@ alias t="eza --icons --tree"
 alias c="clear"
 alias n="nvim"
 alias ..="cd .."
-alias ds="doas pacman -S"
-alias dr="doas pacman -Rns"
-alias dsy="doas pacman -Syu"
+alias ds="sudo dnf install"
+alias dr="sudo dnf remove"
+alias dsy="sudo dnf update"
 alias m="mkdir"
+alias s="sudo"
+#alias fastfetch="~/.config/scripts/fastfetch.sh"
+
+set -x SUDO_PROMPT "pass pls: "
