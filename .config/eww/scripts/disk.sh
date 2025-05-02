960ecd07bb0713a46d3b676c6f29d8c3f494ea80
b65t <iislamm6s7s5@tutamail.com>
2025-05-02 00:46:50 +0000

    Delete .config/eww directory

--- .config/eww/scripts/disk.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-printf "%.0f\n" $(df -h / | awk 'NR==2 {print $5}' | tr -d '%')
--- .config/eww/scripts/start.sh
+++
@@ -1,8 +0,0 @@
-eww open bar
-eww open rrradius
-eww open tra-top
-eww open tra-bottom
-eww open notifications_popup
-eww open ddtime
-eww open dock
-python ~/.config/eww/modules/notifications/notifications.py
