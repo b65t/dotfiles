ce54125d090cdffafe7934fe2f1ea5a8305b2653
b65t <iislamm6s7s5@tutamail.com>
2025-05-09 14:23:44 +0000

    Add files via upload

---
+++ .config/eww/scripts/start.sh
@@ -0,0 +1,10 @@
+eww daemon
+eww open bar
+eww open wallpaper
+eww open rrradius
+eww open tra-top
+eww open tra-bottom
+eww open notifications_popup
+eww open ddtime
+eww open dock
+python ~/.config/eww/yuck/notifications/notifications.py
